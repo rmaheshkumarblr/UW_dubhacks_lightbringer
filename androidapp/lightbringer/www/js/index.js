df3ee5fe83ec37401a0bc7c16d86d7c06738cbe2
Saurabh Sood <saurabhsood91@gmail.com>
2016-10-15 20:41:06 -0700

    capture image and get base64 string

--- androidapp/lightbringer/www/js/index.js
+++ androidapp/lightbringer/www/js/index.js
@@ -33,18 +33,17 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        if(navigator.camera) {
+            app.getPicture();
+        }
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    getPicture: function() {
+        navigator.camera.getPicture(app.gotPicture, app.getPictureFail, {
+            destinationType: Camera.DestinationType.DATA_URL
+        });
+    },
+    gotPicture: function(imageData) {
+        console.log(imageData);
     }
 };
 
